02dc2e69233689265bfaf5283585a3e2d024491f
Bryan Fidello <bryfid857@gmail.com>
2024-10-06 18:08:45 +0700

    appbar

---
+++ lib/widgets/app_bar/appbar_title_searchview.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../../core/app_export.dart';
+import '../custom_search_view.dart';
+
+class AppbarTitleSearchview extends StatelessWidget {
+  AppbarTitleSearchview({Key? key, this.hintText, this.controller, this.margin})
+      : super(
+          key: key,
+        );
+
+  final String? hintText;
+
+  final TextEditingController? controller;
+
+  final EdgeInsetsGeometry? margin;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: margin ?? EdgeInsets.zero,
+      child: SizedBox(
+        width: double.maxFinite,
+        child: CustomSearchView(
+          controller: controller,
+          hintText: "Search by title, genre, actor",
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: 16.h,
+            vertical: 12.h,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/app_bar/appbar_trailing_iconbutton.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:prototype/core/app_export.dart';
+import '../custom_icon_button.dart';
+
+class AppbarTrailingIconbutton extends StatelessWidget {
+  AppbarTrailingIconbutton({Key? key, this.imagePath, this.margin, this.onTap})
+      : super(
+          key: key,
+        );
+
+  final String? imagePath;
+
+  final Function? onTap;
+
+  final EdgeInsetsGeometry? margin;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: margin ?? EdgeInsets.zero,
+      child: GestureDetector(
+        onTap: () {
+          onTap?.call();
+        },
+        child: CustomIconButton(
+          height: 34.h,
+          width: 34.h,
+          padding: EdgeInsets.all(4.h),
+          child: CustomImageView(
+            imagePath: ImageConstant.imgFrame427319010,
+          ),
+        ),
+      ),
+    );
+  }
+}
